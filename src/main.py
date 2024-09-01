c7d95d4c63cb09343bb8bb8008d3db01627cbe05
Vignesh Sekar <vigneshsekar1996@gmail.com>
2024-09-01 23:42:58 +0530

    added static to public folder copy function in main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,34 @@
-from leafnode import LeafNode
+from os import getcwd, listdir, pardir, mkdir 
+from os.path import abspath, join, exists, isfile, isdir
+from shutil import rmtree, copy
 
-def text_node_to_html_node(text_node):
-    text_type = ["text", "bold", "italic", "code", "link", "image"]
-    if text_node not in text_type:
-        raise ValueError("text type is not valid")
-    switch text_node:
-        case "text":
+def copy_static_to_public():
+    cwd = getcwd()
+    basedir = abspath(cwd)
+    static_filepath = join(basedir, "static")
+    public_filepath = join(basedir, "public")
+    if not exists(static_filepath):
+        basedir = abspath(join(cwd, pardir))
+        static_filepath = join(basedir, "static")
+        public_filepath = join(basedir, "public")
+        if not exists(static_filepath):
+            raise Exception("static filepath does not exists.")
+    if exists(public_filepath):
+        rmtree(public_filepath)
+    mkdir(public_filepath)
+    __copy_dir(src_dirpath=static_filepath, dst_dirpath=public_filepath)
 
+def __copy_dir(src_dirpath: str, dst_dirpath) -> None:
+    for file_folder in listdir(path=src_dirpath):
+        content_fullpath = join(src_dirpath, file_folder)
+        if isfile(content_fullpath):
+            if not exists(dst_dirpath):
+                mkdir(dst_dirpath)
+            copy(src = content_fullpath, dst = dst_dirpath)
+        elif isdir(content_fullpath):
+            dest_dir = join(dst_dirpath, file_folder)
+            __copy_dir(content_fullpath, dest_dir)
+    
 
+if __name__ == "__main__":
+    copy_static_to_public()
